bbf749ddf7f49e44b5c747840c014db4377250ed
akramhossain49119 <akram49119@gmail.com>
2020-10-30 00:41:29 +0600

    Add Project DropDownlis

---
+++ CoreDropDownList/CoreDropDownList/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreDropDownList.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
